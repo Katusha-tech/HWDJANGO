9b0716f8843523b8066dfd97393694bace6f2398
Ekaterina <katusha158@rambler.ru>
2025-05-06 22:43:30 +0300

    hw_40: реализовала поисковую форму, которая работает через Q-объекты, изменила класс 'orders_list'

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from .models import Master, Service, Review, Order
 from django.http import HttpResponse
 from .data import * 
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 def landing(request):
     masters = Master.objects.all()
@@ -28,11 +29,48 @@ def thanks(request):
 
 @login_required
 def orders_list(request):
-    orders = Order.objects.all().prefetch_related('services').order_by('-date_created')
+    all_orders = Order.objects.all()
+    search_query = request.GET.get('search', None)
+    check_boxes = request.GET.getlist('search_in')
+    
+    if not check_boxes:
+        check_boxes = ['name']
+
+    if search_query:
+        filters = Q()
+        
+        if "phone" in check_boxes:
+            filters |= Q(phone__icontains=search_query)
+
+        if "name" in check_boxes:
+            filters |= Q(client_name__icontains=search_query)
+
+        if "status" in check_boxes:
+            # Поиск по отображаемым значениям статуса
+            status_display_map = dict(Order.STATUS_CHOICES)
+            matching_status_codes = []
+            
+            # Ищем коды статусов, у которых отображаемое значение содержит поисковый запрос
+            for code, display in Order.STATUS_CHOICES:
+                if search_query.lower() in display.lower():
+                    matching_status_codes.append(code)
+            
+            # Если найдены совпадения, добавляем их в фильтр
+            if matching_status_codes:
+                filters |= Q(status__in=matching_status_codes)
+            else:
+                # Если нет совпадений по отображаемым значениям, 
+                # пробуем искать по коду статуса (для администраторов)
+                filters |= Q(status__icontains=search_query)
+
+        all_orders = all_orders.filter(filters)
+
     context = {
-        'orders': orders
+        'title': 'Список заказов',
+        'orders': all_orders,
     }
     return render(request, 'core/orders_list.html', context)
+            
 
 @login_required
 def order_detail(request, order_id: int):
