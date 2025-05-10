abbbb93fd6955af81ae0c5660df14110fb7e169d
Ekaterina <katusha158@rambler.ru>
2025-05-10 19:17:26 +0300

    hw_41: проанализировла service и внесла измениния в такие поля, как: name и price - обязательные; duration по умолчанию 60 минут, в image добавила 'null=True'

--- core/models.py
+++ core/models.py
@@ -5,9 +5,9 @@ class Service(models.Model):
     name = models.CharField(max_length=200, verbose_name="Название")
     description = models.TextField(blank=True, verbose_name="Описание")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
-    duration = models.PositiveIntegerField(verbose_name="Длительность", help_text="Время выполнения в минутах")
+    duration = models.PositiveIntegerField(verbose_name="Длительность", help_text="Время выполнения в минутах", default=60)
     is_popular = models.BooleanField(default=False, verbose_name="Популярная услуга")
-    image = models.ImageField(upload_to="services/", blank=True, verbose_name="Изображение")
+    image = models.ImageField(upload_to="services/", blank=True, null=True, verbose_name="Изображение")
 
     def __str__(self):
         return self.name
@@ -102,12 +102,3 @@ class Review(models.Model):
             models.Index(fields=['is_published']),
             models.Index(fields=['master','rating'], name='master_rating_idx'),
         ]
-    
-
-
-
-
-
-
-
-
